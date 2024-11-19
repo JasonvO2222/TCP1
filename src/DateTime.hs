031d5ae7a6b40cf98e46d92ef5d76802c04ae9c9
Jason vO <ja.van.otterlo@gmail.com>
2024-11-19 12:57:04 +0100

    beginsel opdracht 1 en 2

--- src/DateTime.hs
+++ src/DateTime.hs
@@ -29,11 +29,45 @@ newtype Second = Second { runSecond :: Int } deriving (Eq, Ord)
 
 -- Exercise 1
 parseDateTime :: Parser Char DateTime
-parseDateTime = undefined
+parseDateTime = DateTime <$> pDate <*> pTime <*> pUTC
+
+--Date parse section
+pDate :: Parser Char Date
+pDate = Date <$> pYear <*> pMonth <*> pDay
+
+pYear :: Parser Char Year
+pYear = (\a b c d -> Year (a * 1000 + b * 100 + c * 10 + d) ) <$> pDig <*> pDig <*> pDig <*> pDig
+
+pMonth :: Parser Char Month
+pMonth = (\a b -> Month (a*10 + b) ) <$> pDig <*> pDig
+
+pDay :: Parser Char Day
+pDay = (\a b -> Day (a*10 + b) ) <$> pDig <*> pDig
+
+--Time parse section
+pTime :: Parser Char Time
+pTime = undefined
+
+
+pUTC :: Parser Char Bool
+pUTC = undefined --(\x -> False) <$> <|> (\x -> True) <$> symbol 'Z'
+
+pDig :: Parser Char Int
+pDig =  digitToInt <$> satisfy isDig
+
+isDig :: Char -> Bool
+isDig = undefined
+
+digitToInt :: Char -> Int
+digitToInt = undefined
 
 -- Exercise 2
 run :: Parser a b -> [a] -> Maybe b
-run = undefined
+run parser xs = do
+    let r = parse parser xs
+    if null r
+    then Nothing
+    else Just $ (fst . head) r
 
 -- Exercise 3
 printDateTime :: DateTime -> String
