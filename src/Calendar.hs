af6a5218b270001233c615b6be99fc1439223ab8
Jason vO <ja.van.otterlo@gmail.com>
2024-11-21 17:41:15 +0100

    begin type framework

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -5,11 +5,27 @@ import DateTime
 
 
 -- Exercise 6
-data Calendar = Calendar
+data Calendar = Calendar { calprops :: [Calprop],
+                           events :: [Event]     }
     deriving (Eq, Ord, Show)
 
-data Event = Event
+data Calprop = Calprop deriving (Eq, Ord, Show)
+
+newtype Prodid = Prodid   { prodid :: String } deriving (Eq, Ord, Show)
+newtype Version = Version { version :: Float } deriving (Eq, Ord, Show)
+
+data Event = Events { eventprops :: [Eventprop] }
     deriving (Eq, Ord, Show)
+
+data Eventprop = Evenprop deriving (Eq, Ord, Show)
+
+newtype DTstamp     = DTstamp     { dtstamp :: DateTime }   deriving (Eq, Ord)
+newtype DTstart     = DTstart     { dtstart :: DateTime }   deriving (Eq, Ord)
+newtype DTend       = DTend       { dtend :: DateTime }     deriving (Eq, Ord)
+newtype UID         = UID         { uid :: String }         deriving (Eq, Ord, Show)
+newtype Description = Description { description :: String } deriving (Eq, Ord, Show)
+newtype Summary     = Summary     { summary :: String }     deriving (Eq, Ord, Show)
+newtype Location    = Location    { location :: String }    deriving (Eq, Ord, Show)
 
 -- Exercise 7
 data Token = Token
