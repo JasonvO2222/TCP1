8f1c88ed87952fd76ee585b73b840ebecfa902e6
GijsKUU <g.c.koppenberg@students.uu.nl>
2024-11-28 19:22:04 +0100

    paar kleine comments in features

--- src/Features.hs
+++ src/Features.hs
@@ -43,11 +43,13 @@ isOverlap e1 e2 = (e1start < e2end && e1end > e2start)
 timeSpent :: String -> Calendar -> Int
 timeSpent s (Calendar cps eps) = sum (map getDuration (iterateEvents eps s))
 
+-- Duration of a single event
 getDuration :: Event -> Int
 getDuration e = dateDiff startD endD + timeDiff startT endT
       where (DateTime startD startT sb) = getStartTime e
             (DateTime endD endT se) = getEndTime e
 
+-- Difference between 2 times
 timeDiff :: Time -> Time -> Int
 timeDiff s@(Time ah am as) e@(Time bh bm bs) = (runHour bh * 3600 + runMinute bm * 60 + runSecond bs) - (runHour ah * 3600 + runMinute am * 60 + runSecond as)
 
@@ -65,6 +67,7 @@ nextDay (Date y m d) | (runDay d) < daysInMonth = Date y m (Day (runDay d + 1))
                where leap = runYear y `mod` 4 == 0
                      daysInMonth = let im = runMonth m in helpDay leap im (even im)
 
+-- go over all events and compare their summary to the given summary
 iterateEvents :: [Event] -> String -> [Event]
 iterateEvents es s = filter (compareSummary s) es
 
